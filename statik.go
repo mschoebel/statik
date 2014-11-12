e6a16c9a2856c12474df01618297ba984b3b8303
Michael Schoebel <mmschoebel@gmail.com>
2014-11-12 21:55:44 +0100

    Adapted import

--- statik.go
+++ statik.go
@@ -121,7 +121,7 @@ func generateSource(srcPath string) (file *os.File, err error) {
 	fmt.Fprintf(&qb, `package %s
 
 import (
-		"github.com/rakyll/statik/fs"
+		"github.com/mschoebel/statik/fs"
 )
 
 func init() {
